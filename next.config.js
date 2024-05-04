3f238e20a622d0d4c80680c8b218be46b665f825
citux <cituxh@gmail.com>
2024-05-03 22:29:04 -0600

    generando pagina

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: "/landinNexuco",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+
+  
 
 module.exports = nextConfig
